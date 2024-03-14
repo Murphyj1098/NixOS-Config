a04e53eb9e25e4562f89c3ce3bc9197ae325e579
Murphyj1098 <jrmurphy@wpi.edu>
2024-03-14 11:08:14 -0400

    Reformat and fix NetworkManager dispatcher script

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -1,4 +1,4 @@
-{ systemSettings, ... }:
+{ pkgs, systemSettings, ... }:
 
 {
   networking = {
@@ -6,12 +6,38 @@
     firewall.enable = false;
     networkmanager = {
       enable = true;
-      # dispatcherScripts = [
-      #   {
-      #     type = "basic";
-      #     source = ./scripts/wlan_docking.sh;
-      #   }
-      # ];
+      dispatcherScripts = [
+        {
+          type = "basic";
+          source = pkgs.writeText "wlan_docking" ''
+          #!/usr/bin/env ${pkgs.bash}/bin/bash
+
+          wired_interfaces="en.*|eth.*"
+          exclude="enp0s13f0u1u4u3"
+
+          # Lab dock (NixOS ifname):  enp0s13f0u1u4u3
+
+          if [[ "$1" =~ $wired_interfaces ]]; then
+              if ! [[ "$1" =~ $exclude ]]; then
+                  case "$2" in
+                      up)
+                          logger "Disabling Wi-Fi"
+                          nmcli radio wifi off
+                          ;;
+                      down)
+                          logger "Enabling Wi-Fi"
+                          nmcli radio wifi on
+                          ;;
+                  esac
+              else
+                  logger "Ethernet interface is on exclude list"
+              fi
+          else
+              logger "Interface is not Ethernet"
+          fi
+          '';
+        }
+      ];
     };
   };
 }
