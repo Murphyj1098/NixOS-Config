fa0ed1ad95827529dffe842daa074543152e7e06
Murphyj1098 <jrmurphy@wpi.edu>
2024-05-20 22:18:50 -0400

    Fix SMB share permission issues

--- system/hardware/cifs-mount.nix
+++ system/hardware/cifs-mount.nix
@@ -8,9 +8,10 @@
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.automount,noauto,nofail,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      user_opts = "iocharset=utf8,vers=3,uid=1000,gid=100";
 
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    in ["${user_opts},${automount_opts},credentials=/etc/nixos/smb-secrets,_netdev"];
   };
 
   fileSystems."/mnt/personal" = {
@@ -18,8 +19,9 @@
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.automount,noauto,nofail,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      user_opts = "iocharset=utf8,vers=3,uid=1000,gid=100";
 
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    in ["${user_opts},${automount_opts},credentials=/etc/nixos/smb-secrets,_netdev"];
   };
 }
